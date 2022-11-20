096a43a36d461a5d65ff702e5b69dafef461188d
Patipan.m <114389220+flooksan@users.noreply.github.com>
2022-11-20 22:16:29 +0700

    Add work shop 2

--- workshop2/src/App.js
+++ workshop2/src/App.js
@@ -1,9 +1,27 @@
+import React, { useState, createContext } from 'react'
+import { Menu,Quiz,Score } from './components'
+import './App.css'
 
-import './App.css';
+
+// create context api  
+export const DataContext = createContext()
 
 function App() {
+  const [appState,setAppState] = useState('menu')
+  const [score,setScore] = useState(0)
+  //เอา score,setScore โยนไปให้ quiz ใช้เพื่อนับคะแนน
+  
   return (
-    <div>Kloof</div>
+    // datacontext ส่ง valueไปมี appState กับ ฟังก์ชัน setAppState
+    <DataContext.Provider value={{appState,setAppState,score,setScore}}>
+      <div className="app">
+        <h1>Web Development Quiz</h1>
+        {/* การจะแสดงผลขึ้นกับ state ของ appState */}
+        {appState === 'menu' && <Menu />}
+        {appState === 'quiz' && <Quiz />}
+        {appState === 'score' && <Score />}
+      </div>
+    </DataContext.Provider>
   );
 }
 
